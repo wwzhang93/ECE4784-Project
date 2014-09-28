95500dcbb3d4296528605d29b585d0380ab8b19a
wwzhang93 <wwzhang93@gmail.com>
2014-09-28 19:39:09 -0400

    Updated projPhase1.m
    Implemented loop for Euler's method.

--- projPhase1.m
+++ projPhase1.m
@@ -3,40 +3,71 @@
 %William Zhang
 %Due September, 29th 2014
 
-%Constants Provided:
+%%Constants Provided:
 simTtot = 100; %100 ms total simulation time
-t = 0:.01:simTtot; %create time vector using .01 steps between data points
+step = .02;
+t = 0 : step : simTtot; %create time vector using .01 steps between data points
 
-gK = 36; %36 mS/cm^2
-gNa = 120; %120 mS/cm^2
-gL = 0.3; %0.3 mS/cm^2
+%Maximum Conductances
+gKBAR = 36; %36 mS/cm^2
+gNaBAR = 120; %120 mS/cm^2
+gLBAR = 0.3; %0.3 mS/cm^2
 EK = -12; %-12 mV
 ENa = 115; %115 mV
 EL = 10.6; %10.6 mV
 VRest = -70; %-70 mV resting potential of membrane.
+Cm = 1.0; %uF/cm^2
 
-%Equations
+%%Equations
 %Gating Variables:
-V = 0; %Initial assumption that V = 0;
-alpham = 0.1.*((25-V)/(exp((25-V)/10)-1));
-betam = 4*exp(-V/18);
-alphan = 0.01.*((10-V)/(exp((10-V)/10)-1));
-betan = 0.125*exp(-V/80);
-alphah = 0.07*exp(-V/20);
-betah = 1/((exp(30-V)/10)+1);
+V(1) = 0; %Initial assumption that V = 0;
+alpham(1) = 0.1.*((25-V(1))/(exp((25-V(1))/10)-1));
+betam(1) = 4*exp(-V(1)/18);
+alphan(1) = 0.01.*((10-V(1))/(exp((10-V(1))/10)-1));
+betan(1) = 0.125*exp(-V(1)/80);
+alphah(1) = 0.07*exp(-V(1)/20);
+betah(1) = 1/((exp(30-V(1))/10)+1);
 
 %Initial Values:
-m(1) = alpham/(alpham-betam);
-n(1) = alphan/(alphan-betan);
-h(1) = alphah/(alphah-betah);
+m(1) = alpham(1)/(alpham(1)+betam(1));
+n(1) = alphan(1)/(alphan(1)+betan(1));
+h(1) = alphah(1)/(alphah(1)+betah(1));
 
-%Currents:
-% I = zeros(length(t));
-%I(1:50) = 5;
-%curStim = 10;
-%I = ones(length(t)) .* curStim;
+%%Injected Current
+amp = input('Enter the amplitude of the injected current (in uA/cm^2): ');
+dur = input('Enter the duration of the injected current [between 0 and 100 ms]: ');
+I = zeros(1, length(t)); % Setting up I to be the same size as the time array
+for y = 1:dur/step % injected current goes from 0 to (duration entered / time step), elsewhere is 0
+I(y) = amp;
+end
 
-%iNa = m.^3.*gNa.*h.*(V-ENa);
-%iK = n.^4*gK*(V-EK);
-%iL = gL*(V-EL);
-%iIon = I - iK - iNa - iL;
+
+for j = 1 : length(t)-1
+    alpham = 0.1.*((25-V(j))/(exp((25-V(j))/10)-1));
+    betam = 4*exp(-V(j)/18);
+    alphan = 0.01.*((10-V(j))/(exp((10-V(j))/10)-1));
+    betan = 0.125*exp(-V(j)/80);
+    alphah = 0.07*exp(-V(j)/20);
+    betah = 1/((exp(30-V(j))/10)+1);
+    
+    gK(j) = n(j)^4*gKBAR;
+    gNa(j) = m(j)^3*gNaBAR *h(j);
+    gL(j) = gLBAR;
+    
+    iK(j) = gK(j)*(V(j)-EK);
+    iNa(j) = gNa(j)*(V(j)-ENa);
+    iL(j) = gL(j)*(V(j)-EL);
+    iIon(j) = I(j)-iNa(j)-iK(j)-iL(j);
+    
+    derivm = alpham*(1-m(j))-betam*m(j);
+    derivn = alphan*(1-n(j))-betan*n(j);
+    derivh = alphah*(1-h(j))-betah*h(j);
+    
+    m(j+1) = m(j)+step.*derivm;
+    n(j+1) = n(j)+step.*derivn;
+    h(j+1) = h(j)+step.*derivh;
+    
+    derivV(j) = iIon(j)/Cm;
+    V(j+1) = V(j)+step*derivV(j);
+    
+end
